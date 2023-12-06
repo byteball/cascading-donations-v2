9150b5b4c75c812314bc60a9518758edc3292875
tony <tonyofbyteball@users.noreply.github.com>
2023-12-06 19:27:28 +0400

    link to discord bot

--- src/app/donors/page.tsx
+++ src/app/donors/page.tsx
@@ -3,6 +3,7 @@ import { Metadata } from "next";
 
 import { SubTitle, Title } from "@/components";
 import { ListOfDonors } from "@/components/ListOfDonors/ListOfDonors";
+import appConfig from "@/appConfig";
 
 export const metadata: Metadata = {
   title: 'Kivach - List of donors',
@@ -17,6 +18,10 @@ export default () => (<div className="mt-12 relative">
 
 	<SubTitle level={1} className="mt-6">
 		If you want to show your nick instead of your address, please <Link href="set_nick" className="text-primary">set your nick</Link>.
+	</SubTitle>
+
+	<SubTitle level={1} className="mt-6">
+		You can also get a special status in the <a href="https://discord.obyte.org" target="_blank" rel="noopener" className="text-primary">Obyte discord server</a> depending on the total amount of your donations. To do so, link your Obyte address to your discord username using <a href={"obyte:" +appConfig.BOT_PAIRING_CODE} className="text-primary">Kivach discord bot</a>.
 	</SubTitle>
 
 	<ListOfDonors />
